113eab935aa27367a6a0f62e828e164a07b68221
Michal Szukala <szukalagithub@gmail.com>
2017-12-08 23:01:55 +0100

    Task class

---
+++ ToDoList/Task.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList
+{
+    class Task
+    {
+        private DateTime date;
+        private string description;
+        private PriorityUnit priority;
+
+        //All of constractors
+        public Task() : this(DateTime.Now, "Unknow", PriorityUnit.important)
+        {
+        }
+        
+        public Task(DateTime date, string description, PriorityUnit priority)
+        {
+            this.date = date;
+            this.description = description;
+            this.priority = priority;
+        }
+
+        //All of properties
+        public string Description
+        {
+            get { return description; }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                    description = value;
+            }
+        }
+        
+        public PriorityUnit PriorityUnit
+        {
+            get { return priority; }
+            set
+            {
+                if (Enum.IsDefined(typeof(PriorityUnit), value))
+                    priority = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            string textOut = string.Empty;
+            textOut = string.Format("{0, -40} {1, 6:f2} {2, -6}", date, priority, description);
+            return textOut;
+        }
+
+    }
+}
